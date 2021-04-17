26669a6d5ea2e003d98d1422780485a021f0f8f9
Winstone <mjulewinstone2@gmail.com>
2021-04-18 02:36:01 +0300

    Edit Config file

--- newsportal/admin/includes/config.php
+++ newsportal/admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','newsportal');
+define('DB_SERVER','us-cdbr-east-03.cleardb.com');
+define('DB_USER','b786b85bb244b4');
+define('DB_PASS' ,'9c9e283c');
+define('DB_NAME','heroku_c6f35d087a69174');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
